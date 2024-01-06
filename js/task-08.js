abab5f8c6f17bbab554116144671b55c0e60addb
czke33 <czke33@gmail.com>
2024-01-06 20:22:10 +0200

    task 8 and 9

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,19 @@
+const formLabel = document.querySelector(".login-form");
+formLabel.addEventListener("submit", (event) => {
+  event.preventDefault();
 
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+  if (email.value === "" || password.value === "") {
+    return alert("Please fill in all the fields!");
+  }
+
+  const elementsObj = {
+    email: email.value,
+    password: password.value,
+  };
+
+  event.currentTarget.reset();
+  return console.log(elementsObj);
+});
--- js/task-09.js
+++ js/task-09.js
@@ -1,5 +1,19 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
+  let randomColor = `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
+  console.log(randomColor);
+  return randomColor;
 }
+
+const bodyColor = document.querySelector("body");
+const button = document.querySelector(".change-color");
+const nameColor = document.querySelector(".color");
+
+const changeColor = () => {
+  bodyColor.style.backgroundColor = getRandomHexColor();
+  nameColor.textContent = getRandomHexColor();
+};
+
+window.onload = (event) => changeColor();
+button.addEventListener("click", changeColor);
